1751d44d99095c6dcfc67a2e9e15cd3e9a2a631e
Akshay-dot123 <akshaytest123@gmail.com>
2024-12-19 14:30:53 +0530

    Middleware Applied

--- src/app.js
+++ src/app.js
@@ -1,19 +1,48 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
+const adminAuth = require("./middlewares/auth");
+// Middleware
+app.use("/admin", adminAuth);
 
+app.get("/admin/user", (req, res) => {
+  res.send("User data sent");
+});
+
+// Multiple Route Handler
+app.use(
+  "/route",
+  (req, res, next) => {
+    console.log("Handling route 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling route 2");
+    res.send("2nd response !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling route 2");
+    res.send("2nd response !!");
+  }
+);
+
+// Regex
+app.get("/abc(de)?d", (req, res) => {
+  res.send({ firstName: "Akshay" });
+});
 // This will only handle GET call to /user
-app.get("/user",()=>{
-    res.send({firstName:'Akshay'})
-})
+app.get("/*user$/", (req, res) => {
+  res.send({ firstName: "Akshay" });
+});
 
-app.post("/user",()=>{
-    res.send("Data saved successfully")
-})
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully");
+});
 
-// This will match all HTTP requests
-app.use("/hello",(req,res)=>{
-    res.send("Hello from server")
-})
-app.listen(3000,()=>{
-    console.log("Listen to PORT:3000");
-})
+// This will match all HTTP requests like Get, Post and all
+app.use("/hello", (req, res) => {
+  res.send("Hello from server");
+});
+app.listen(3000, () => {
+  console.log("Listen to PORT:3000");
+});
---
+++ src/middlewares/auth.js
@@ -0,0 +1,10 @@
+const adminAuth=(req,res,next)=>{
+    let token ="abc"
+    let AdminAuth= token ==="abc"
+    if(!AdminAuth){
+        res.status(201).send("Unauthorized")
+    }else{
+        next()
+    }
+}
+module.exports=adminAuth
